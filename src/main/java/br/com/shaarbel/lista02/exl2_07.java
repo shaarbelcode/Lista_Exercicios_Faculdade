806208c6ddf30cf537c4d9ae1a80cdb2bad8f649
shaarbelcode <shaarbelbreis@gmail.com>
2026-09-10 19:15:36 -0400

    Segunda lista de exercicios

--- src/main/java/br/com/shaarbel/lista02/exl2_07.java
+++ src/main/java/br/com/shaarbel/lista02/exl2_07.java
@@ -16,5 +16,14 @@ public class exl2_07 {
         System.out.println("Me informe as horas");
         int hora = sc.nextInt();
 
+        if(hora >= 0 && hora <=11){
+            System.out.println("Bom diaaaa !!!");
+        }else if(hora >= 12 && hora<=17){
+            System.out.println("Boa tarde fiot ");
+        }else if(hora >18 && hora <=23){
+            System.out.println("Boa noite meu consagrado ");
+        }else{
+            System.out.println("FORMATO DE HORAS INFORMADO INCORRETO , PORFAVOR INSERIR MODO DE HORARIO CORRETO. ");
+        }
     }
 }
