61f7298a03ca7d3d34c39ec65b7bacefc6bc09f9
shaarbelcode <shaarbelbreis@gmail.com>
2026-09-13 21:47:06 -0400

    Segunda lista exercicios

--- src/main/java/br/com/shaarbel/lista02/exl2_02.java
+++ src/main/java/br/com/shaarbel/lista02/exl2_02.java
@@ -1,4 +1,18 @@
+//2. Ler um número inteiro e imprimir uma linha para cada
+//característica verdadeira: é positivo, é par, é múltiplo de 5, tem
+//dois dígitos. Para a contagem de dígitos, considere o módulo do
+//número, obtido com Math.abs(numero). Se nenhuma
+//característica for verdadeira, nada é impresso
+
 package br.com.shaarbel.lista02;
 
+import java.util.Scanner;
+
 public class exl2_02 {
+    public void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite seu numero : ");
+        int numero = sc.nextInt();
+    }
 }
