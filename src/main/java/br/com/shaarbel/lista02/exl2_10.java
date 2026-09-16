9e79b8aa35063e1a82097ccfaebb99578ab8852c
shaarbelcode <shaarbelbreis@gmail.com>
2026-09-16 17:32:40 -0400

    Exercicio Java

---
+++ src/main/java/br/com/shaarbel/lista02/exl2_10.java
@@ -0,0 +1,20 @@
+//10. Ler o nome de usuário e a senha. Se o usuário for diferente
+//de admin, imprimir que o usuário não foi encontrado e não
+//verificar a senha. Se o usuário existir, verificar a senha: se for
+//java123, o acesso é liberado; caso contrário, a senha está
+//incorreta. São três saídas diferentes
+
+
+
+package br.com.shaarbel.lista02;
+
+import java.util.Scanner;
+
+public class exl2_10 {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String usuario, senha;
+
+        System.out.println("Digite seu usuário ");
+    }
+}
